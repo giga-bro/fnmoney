887b84ee62340e963f2afdea1b0ac321fbc5187b
giga-bro <aditya.sakpal2008@gmail.com>
2024-07-22 20:10:44 +0530

    Update Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,13 +4,13 @@ const Main:React.FC = () => {
   return (
     <>
     <div className="w-[100vw] h-[90vh] bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center " >
-        <div className='  w-[90%] h-[60%] bg-[#171926] md:opacity-95 md:w-[40%] lg:w-[20%] lg:h-[60%] rounded-2xl lg:p-[2%] ' >
+        <div className='  w-[90%] h-[60%] bg-[#171926] md:opacity-95 md:w-[40%] lg:w-[20%] lg:h-[60%] rounded-2xl ' >
             <div className='w-[100%] h-[60%]   text-wrap flex flex-col justify-center items-center ' >
-                <p className='text-wrap text-2xl text-center text-white font-bold w-[100%] lg:text-4xl  ' >Welcome To</p>
-                <p className='text-wrap text-4xl text-center text-white font-bold w-[100%] lg:text-6xl ' >FNAXIOM</p>
-                <p className='text-wrap text-3xl text-center text-white font-bold w-[100%] lg:text-5xl  ' >Full Stack</p>
-                <p className='text-wrap text-3xl text-center text-white font-bold w-[100%] lg:text-5xl ' >Intership</p>
-                <p className='text-wrap text-3xl text-center text-white font-bold w-[100%] lg:text-5xl ' >Assesment</p>
+                <p className='text-wrap text-xl text-center text-white font-bold w-[100%] lg:text-4xl  ' >Welcome To</p>
+                <p className='text-wrap text-3xl text-center text-white font-bold w-[100%] lg:text-6xl ' >FNAXIOM</p>
+                <p className='text-wrap text-2xl text-center text-white font-bold w-[100%] lg:text-5xl  ' >Full Stack</p>
+                <p className='text-wrap text-2xl text-center text-white font-bold w-[100%] lg:text-5xl ' >Intership</p>
+                <p className='text-wrap text-2xl text-center text-white font-bold w-[100%] lg:text-5xl ' >Assesment</p>
             </div>
             <div className='w-[100%] h-[40%] flex flex-col justify-between items-center  py-[5%] ' >
                 <button className='text-white bg-[#94b2ed] rounded-lg w-[90%] h-[35%] text-lg font-bold  ' >Enroll HERE</button>
